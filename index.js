ac30bb30f7faa3afa6464a578d608739bc2bcb3a
Carlo Valdivia Luna <cvaldivialu@unsa.edu.pe>
2025-03-15 19:24:08 -0500

    agrega una nota

--- index.js
+++ index.js
@@ -26,6 +26,12 @@ let Notas = [
     content: 'GET and POST are the most important methods of HTTP protocol',
     date: '2019-05-30T19:20:14.298Z',
     important: true
+  },
+  {
+    id: 4,
+    content: 'HTTP is a protocol',
+    date: '2019-05-30T19:20:14.298Z',
+    important: true
   }
 ]
 
